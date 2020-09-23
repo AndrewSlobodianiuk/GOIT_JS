d875f32e8b129f737191babfd1b2b727cb44839b
Andrii Slobodianiuk <ASlobodianiuk@on.seo>
2020-09-23 14:46:29 +0300

    add module 11

---
+++ test/script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const articles = [
+  {
+    title: "123",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+    date: "321",
+    comments: [{ comment: 'coment' }, { comment: 'coment' }],
+    category: 'politics'
+  },
+  {
+    title: "123",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+    date: "321",
+    comments: [{ comment: 'coment' }, { comment: 'coment' }, { comment: 'coment' }],
+    category: 'politics'
+  },
+  {
+    title: "123",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+    date: "321",
+    comments: [{ comment: 'coment' }, { comment: 'coment' }, { comment: 'coment' }],
+    category: 'car'
+  },
+  {
+    title: "123",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+    date: "321",
+    comments: [{ comment: 'coment' }, { comment: 'coment' }, { comment: 'coment' }],
+    category: 'car'
+  },
+  {
+    title: "123",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+    date: "321",
+    comments: [{ comment: 'coment' }, { comment: 'coment' }, { comment: 'coment' }],
+    category: 'car'
+  },
+  {
+    title: "123",
+    article:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, asperiores optio obcaecati illum nobis aspernatur, suscipit numquam odio quaerat aperiam magni! Aut laboriosam tempore quis placeat esse non impedi aspernatur!",
+    date: "321",
+    comments: [{ comment: 'coment' }, { comment: 'coment' }, { comment: 'coment' }],
+    category: 'lifestyle'
+  },
+];
+
+
+// Фукционал
+//  - Пользователь может ввести свое имя и фамилию в форму и отправить ее
+//  - После отправки, под формой вы должны отрендерить надпись {name} {secondName}, 
+// выбирите категорию новостей а также селектор где пользователь может выбрать 1 из 3 категорий новостей
+//  - После выбора категории новости вы должны отрендерить те новости которые выбрал пользователь 
+//  - Пользователь может выбрать другую категорию, вы должны отрендерить новости из новой категории
+// - При перезагрузке страницы имя пользовател и выбранная категория должна сохранятся 
+
+
+
